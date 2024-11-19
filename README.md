20ff136b932334bc73d88ed5d84c0084efc9e41c
ashaforddesigns <ashaforddesigns@gmail.com>
2024-11-18 23:31:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Survivor Hangman Game :heart_on_fire:
+
+## Goals
+My goal for this project is make a game that you can play in the terminal. I wanted to base the words of one of my favorite tv shows, Survivor. (I actually prefer Big Brother, but I felt like Survivor had more words to choose from)
+
+## Process
+
+### Inspiration
+As I continue to lurk around in this community, I realized quickly that hangman is a popular beginner project, and it sounded quite fun and simple enough to me so I found this image as inspiration for it.
+![image of python hangman game inspo](https://code-projects.org/wp-content/uploads/2019/11/Capture.png)
+
+
+and I grabbed [Chris Horton's Hangman images](https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c)
+
+### Gathering the list
+Even when I do graphic design work, I always use ChatGPT for copy and I'm planning on using it here to provide a me a list of Survior words. I could have made it 
+
+### Developing a plan
+
---
+++ surivor_hangman.py
@@ -0,0 +1,67 @@
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+# 6 drawings = 6 lives
+print("Welcome to Survivor Hangman!")
+userLetter = input("Enter your guess: ")
+wordBank = ["Tribal Council", "Hidden Immunity Idol","Merge","Shot in the Dark","Blindside","Alliance","Outwit", "Outplay", "Outlast", "Final Tribal"
+"Reward Challenge"
+"Immunity Challenge"
+"Vote Split"
+"Jury"
+"Castaway"
+"Buff"
+"Torch Snuffed"
+"Rice Rationing"
+"Puzzle Master"
+"Survivor Auction"
+"Camp Life"
+"Social Game"]
