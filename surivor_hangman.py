65ca797c17450169b51022e2962d43be6c3eaeb9
ashaforddesigns <ashaforddesigns@gmail.com>
2024-11-19 00:52:09 -0500

    probably a buggy mess!

--- surivor_hangman.py
+++ surivor_hangman.py
@@ -62,26 +62,23 @@ print(HANGMANPICS[0])
 blank = len(keyword)*"_ "
 print(blank) # prints corresponding underscore for every letter
 lives = 6
+failedTries = 0
 print("You have",lives, "lives remaining")
 
 userLetter = input("Enter your guess: ")
-for letter in keyword: #you guessed right
-    if userLetter == letter: #replaces underscore with letter
-        print()
-
-    
-if userLetter not in usedLetters: #you guessed wrong
-    usedLetters += userLetter
-    print("You have used ", usedLetters)
-elif userLetter in use
-
-
-while lives < 6:
-    print("You have",lives, "lives remaining")
-    userLetter = input("Enter your guess: ")
-    lives -= 1
-
-
-
-if userLetter not in usedLetters:
-    usedLetters += userLetter
+for letter in keyword: 
+    if userLetter == letter: #you guessed right, underscore is replaced and printed
+        blank.replace(keyword[letter], userLetter)
+        print(blank)
+        userLetter = input("Enter your guess: ")
+    if userLetter != letter: #you guessed wrong, new hangman appears
+        usedLetters += userLetter
+        print("You have used ", usedLetters)
+        failedTries+=1
+        print(HANGMANPICS[failedTries])
+        lives -= 1
+        userLetter = input("Enter your guess: ")
+        if lives == 0:
+            print("The word was ", keyword,". The tribe has spoken")
+            break
+# what happens if you win?!
