607f4d7b55fc8ac15fa7f2f7a4c9c1c2cbdf89be
ashaforddesigns <ashaforddesigns@gmail.com>
2024-11-19 14:08:02 -0500

    pain stage

--- surivor_hangman.py
+++ surivor_hangman.py
@@ -50,35 +50,54 @@ HANGMANPICS = ['''
       |
 =========''']
 
-wordBank = ["Council", "Hidden Immunity Idol","Merge","Blindside","Alliance","Outwit", "Outplay", "Outlast", "Tribal", "Reward", "Challenge", "Immunity Challenge","Vote Split"
-, "Jury","Castaway","Buff","Torch Snuffed","Rice Rationing","Puzzle Master","Survivor Auction","Camp Life","Social Game", "Jeff", "Probst","Journey","Parvati", "Cirie"] # 27 items
-
+wordBank = ["Council", "Immunity", "Idol","Merge","Blindside","Alliance","Outwit", "Outplay", "Outlast", "Tribal", "Reward", "Challenge", "Immunity","Vote"
+, "Jury","Castaway","Buff","Torch","Snuffed","Puzzle","Auction","Camp","Social Game", "Jeff", "Probst","Journey","Parvati", "Cirie"] # 27 items
 keyword = random.choice(wordBank)
 # chooses keyword before game begins
-usedLetters = list()
 # 6 drawings = 6 lives
-print("Welcome to Survivor Hangman!")
-print(HANGMANPICS[0])
-blank = len(keyword)*"_ "
-print(blank) # prints corresponding underscore for every letter
+
+#initial settings
+usedLetters = list()
+blanks = list()
 lives = 6
 failedTries = 0
-print("You have",lives, "lives remaining")
 
+#Display
+print("Welcome to Survivor Hangman!")
+print("FOR TESTING PURPOSES: the keyword is", keyword)
+print(HANGMANPICS[0])
+blanks = ["_"]*len(keyword)
+print(blanks)
+print("You have",lives, "lives remaining")
 userLetter = input("Enter your guess: ")
-for letter in keyword: 
-    if userLetter == letter: #you guessed right, underscore is replaced and printed
-        blank.replace(keyword[letter], userLetter)
-        print(blank)
-        userLetter = input("Enter your guess: ")
-    if userLetter != letter: #you guessed wrong, new hangman appears
-        usedLetters += userLetter
-        print("You have used ", usedLetters)
-        failedTries+=1
-        print(HANGMANPICS[failedTries])
-        lives -= 1
-        userLetter = input("Enter your guess: ")
-        if lives == 0:
-            print("The word was ", keyword,". The tribe has spoken")
-            break
+while failedTries < 6:
+  print("You have",lives, "lives remaining")
+  userLetter = input("Enter your guess: ")
+  for index, letter in enumerate(keyword):
+    if userLetter == letter: #when you guess right
+      blanks[index] = userLetter
+      print(blanks)
+    else: #you guessed wrong, new hangman appears
+      usedLetters += userLetter
+      print("You have used ", usedLetters)
+      failedTries+=1
+      print(HANGMANPICS[failedTries])
+      lives -= 1
+
+# for letter in keyword: 
+#     if userLetter == letter: #you guessed right, underscore is replaced and printed
+#         blank.replace(keyword[letter], userLetter) # the correct section is buggy as hell
+#         print(blank)
+#         userLetter = input("Enter your guess: ")
+#     if userLetter != letter: #you guessed wrong, new hangman appears
+#         usedLetters += userLetter
+#         print("You have used ", usedLetters)
+#         failedTries+=1
+#         print(HANGMANPICS[failedTries])
+#         lives -= 1
+#         print(blank)
+#         userLetter = input("Enter your guess: ")
+#         if lives == 0:
+#             print("The word was ", keyword,". The tribe has spoken")
+#             break
 # what happens if you win?!
